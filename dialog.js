71986f1d3255f0d7b2c98cf35d23df568d819c8a
charleslei <SuperWanderMan@gmail.com>
2014-04-22 23:59:24 +0800

    fixed bug

--- dialog.js
+++ dialog.js
@@ -1,19 +1,45 @@
 ﻿(function(win, doc){
-    function dialog(){
-        this.init();
+    function dialog(html){
+        html = html || '';
+        if(!html){
+            return;
+        }
+        var $html = $(html);
+        if($html && $html.length > 0){
+            this._init($html);
+        }
     }
 
     var dl = {
-        init: function(){
-                  var me = this, br = $.browser;
-                  if(br.msie && br.version === '6.0'){
-                      var dh = $(doc).height();
-                      var dw = $(doc).width();
-                      $('.back').css({'position':'absolute', 'left':'0', 'top':'0', 'width':'100%', 'height':dh+'px', 'width':dw+'px'});
-                  }
+        _init: function($html){
+                  var me = this;
                   me._initEvent();
+                  me._initDraw($html);
                   me._resize();
               },
+        _initDraw: function(con){
+                       var me = this;
+                       me.dom = {};
+                       var backDom = $("<div></div>");
+                       backDom.attr('style', 'width:100%;height:100%;left: 0;top: 0;opacity:0.5;position:fixed;background-color:#000;filter:alpha(opacity=50);');
+                       this.dom.backDOM = backDom;
+
+                       var fntDom = $("<div></div>");
+                       fntDom.attr('style', 'display:none;width:auto;height:auto;background-color:#FFF;position:fixed;');
+                       this.dom.fntDOM = fntDom;
+                       fntDom.append(con);
+                       $('body').append(backDom).append(fntDom);
+
+                       //IE6
+                       me._drawIE6();
+                   },
+        _drawIE6: function(){
+                        var me = this, br = $.browser;
+                        if(br.msie && br.version === '6.0'){
+                            //TODO:100% or ?
+                            me.dom.backDOM.css({'position':'absolute'});
+                        }
+                    },
         _initEvent: function(){
                         var me = this;
                         $(win).bind('resize.dialog', function(e){
@@ -22,17 +48,35 @@
                         });
                     },
         _resize: function(){
-                    var ww = $(win).width();
-                    var wh = $(win).height();
+                     var me = this;
+                     var fntDom = me.dom.fntDOM, br = $.browser;
+                     if(!(br.msie && br.version === '6.0')){
+                         var ww = $(win).width();
+                         var wh = $(win).height();
 
-                    var pw = $('.front').width();
-                    var ph = $('.front').height();
-                    var leftP = (ww - pw) / 2;
-                    var topP = (wh - ph) / 2;
-                    $('.front').css({'left':leftP+'px', 'top':topP+'px'});
-                },
+                         var pw = fntDom.width();
+                         var ph = fntDom.height();
+
+                         var leftP = (ww - pw) / 2;
+                         var topP = (wh - ph) / 2;
+                         fntDom.css({'left':leftP+'px', 'top':topP+'px'});
+                     }else{
+                         var dh = $(doc).height();
+                         var dw = $(doc).width();
+                         //TODO:width=100% or ?
+                         me.dom.backDOM.css({'height':dh+'px','width':dw+'px'});
+                         //TODO: front;
+                     }
+                 },
         show: function(){
-                  $('.front').show();
+                  var me = this, dom = me.dom;
+                  dom.fntDOM.show();
+                  dom.backDOM.show();
+              },
+        hide: function(){
+                  var me = this, dom = me.dom;
+                  dom.fntDOM.hide();
+                  dom.backDOM.hide();
               }
     }
 
