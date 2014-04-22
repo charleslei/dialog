2369be58bb454b01fa33d5cef81339833bf6bbc8
charleslei <SuperWanderMan@gmail.com>
2014-04-22 23:11:01 +0800

    format

--- dialog.js
+++ dialog.js
@@ -1,41 +1,43 @@
-﻿$(function(){
+﻿(function(win, doc){
     function dialog(){
         this.init();
     }
 
     var dl = {
         init: function(){
-                  var br = $.browser;
+                  var me = this, br = $.browser;
                   if(br.msie && br.version === '6.0'){
-                      var dh = $(document).height();
-                      var dw = $(document).width();
-                      $('.back').css('position', 'absolute').css('left', '0').css('top', '0').css('width', '100%').css('height',dh + 'px').css('width', dw + 'px');
+                      var dh = $(doc).height();
+                      var dw = $(doc).width();
+                      $('.back').css({'position':'absolute', 'left':'0', 'top':'0', 'width':'100%', 'height':dh+'px', 'width':dw+'px'});
                   }
-                  resize();
+                  me._initEvent();
+                  me._resize();
               },
-_initEvent: function(){
-                $(window).bind('resize.dialog', function(e){
-                    resize();
-                    e.preventDefault();
-                })
-            },
-resize: function(){
-            var ww = $(window).width();
-            var wh = $(window).height();
+        _initEvent: function(){
+                        var me = this;
+                        $(win).bind('resize.dialog', function(e){
+                            me._resize();
+                            e.preventDefault();
+                        });
+                    },
+        _resize: function(){
+                    var ww = $(win).width();
+                    var wh = $(win).height();
 
-            var pw = $('.front').width();
-            var ph = $('.front').height();		
-            var leftP = (ww - pw) / 2;
-            var topP = (wh - ph) / 2;		
-            $('.front').css('left', leftP + 'px').css('top', topP + 'px');
-        },
-show: function(){
-          $('.front').show();
-      }
+                    var pw = $('.front').width();
+                    var ph = $('.front').height();
+                    var leftP = (ww - pw) / 2;
+                    var topP = (wh - ph) / 2;
+                    $('.front').css({'left':leftP+'px', 'top':topP+'px'});
+                },
+        show: function(){
+                  $('.front').show();
+              }
     }
 
     dialog.prototype = dl;
 	
-	window.Dialog = dialog;
+	win.Dialog = dialog;
 
-})
+})(window, document);
