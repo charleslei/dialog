2d7710ee067d3a2f01ca9d28c1eae3d4471645cb
chunlei.zhang <chunlei.zhang@qunar.com>
2014-04-25 14:07:07 +0800

    fixed bug

--- dialog.js
+++ dialog.js
@@ -42,11 +42,11 @@ $(function(){
       var me = this;
       me.dom = {};
       var backDom = $("<div></div>");
-      backDom.attr('style', 'width:100%;height:100%;left: 0;top: 0;opacity:0.5;position:fixed;background-color:#000;filter:alpha(opacity=50);z-index:6666');
+      backDom.attr('style', 'width:100%;height:100%;left: 0;top: 0;opacity:0.5;position:fixed;background-color:#000;_filter:alpha(opacity=50);z-index:6666');
       me.dom.backDOM = backDom;
 
       var fntDom = $("<div></div>");
-      fntDom.attr('style', 'display:none;width:auto;height:auto;background-color:#FFF;position:fixed;z-index:6667');
+      fntDom.attr('style', 'display:none;width:auto;height:auto;position:fixed;z-index:6667');
       me.dom.fntDOM = fntDom;
       fntDom.append(con);
       me.body.append(backDom).append(fntDom);
