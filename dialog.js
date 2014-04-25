ccc6ae25cfc240fe395dfa26721e2bb6cba38e75
chunlei.zhang <chunlei.zhang@qunar.com>
2014-04-25 12:54:08 +0800

    optimize code

--- dialog.js
+++ dialog.js
@@ -1,6 +1,6 @@
-﻿(function(win, doc){
+﻿$(function(){
   function dialog(html){
-    var args = Array.apply(null,arguments);
+    var args = Array.apply(null,arguments), me = this;
     var $html;
     if(args.length === 0) return;
 
@@ -14,21 +14,24 @@
     }
 
     var two = args[1];
-    this.cfg = {
+    me.cfg = {
       onShow: function(){}
     }
     if(two && Object.prototype.toString.call(two) === "[object Function]"){
-      $.extend(this.cfg, {onShow: two});
+      $.extend(me.cfg, {onShow: two});
     }
 
     if($html && $html.length > 0){
-      this._init($html);
+      me._init($html);
     }
   };
 
   var dl = {
     _init: function($html){
       var me = this;
+      me.doc = $(document);
+      me.win = $(window);
+      me.body = $('body');
       me._initEvent();
       me._initDraw($html);
       me._resize();
@@ -39,13 +42,13 @@
       me.dom = {};
       var backDom = $("<div></div>");
       backDom.attr('style', 'width:100%;height:100%;left: 0;top: 0;opacity:0.5;position:fixed;background-color:#000;filter:alpha(opacity=50);z-index:6666');
-      this.dom.backDOM = backDom;
+      me.dom.backDOM = backDom;
 
       var fntDom = $("<div></div>");
       fntDom.attr('style', 'display:none;width:auto;height:auto;background-color:#FFF;position:fixed;z-index:6667');
-      this.dom.fntDOM = fntDom;
+      me.dom.fntDOM = fntDom;
       fntDom.append(con);
-      $('body').append(backDom).append(fntDom);
+      me.body.append(backDom).append(fntDom);
 
       //IE6
       me._drawIE6();
@@ -60,14 +63,14 @@
     },
     _initEvent: function(){
       var me = this, br = $.browser;;
-      $(win).bind('resize.dialog', function(e){
+      me.win.bind('resize.dialog', function(e){
         me._resize();
         e.preventDefault();
       });
 
 
       if(br.msie && br.version === '6.0'){
-        $(window).scroll(function(e){
+        me.win.scroll(function(e){
           me._resize();
           e.preventDefault();
         })
@@ -77,8 +80,8 @@
     _resize: function(){
       var me = this;
       var fntDom = me.dom.fntDOM, backDom = me.dom.backDOM, br = $.browser;
-      var ww = $(win).width();
-      var wh = $(win).height();
+      var ww = me.win.width();
+      var wh = me.win.height();
 
       var pw = fntDom.width();
       var ph = fntDom.height();
@@ -88,11 +91,11 @@
       if(!(br.msie && br.version === '6.0')){
         fntDom.css({'left':leftP+'px', 'top':topP+'px'});
       }else{
-        var dh = $(document.body).outerHeight(true);
-        var dw = $(document.body).outerWidth(true);//浏览器时下窗口文档body的总宽度 包括border padding margin不包括滚动条；
+        var dh = $(me.body).outerHeight(true);
+        var dw = $(me.body).outerWidth(true);//浏览器时下窗口文档body的总宽度 包括border padding margin不包括滚动条；
 
-        var scrollLeft = $(document).scrollLeft();
-        var scrollTop = $(document).scrollTop();
+        var scrollLeft = me.doc.scrollLeft();
+        var scrollTop = me.doc.scrollTop();
         backDom.css({'height':dh + 'px','width':dw + 'px'});
         fntDom.css({'left': leftP + scrollLeft +'px', 'top': topP + scrollTop +'px'});
       }
@@ -121,6 +124,6 @@
 
   dialog.prototype = dl;
 
-  win.Dialog = dialog;
+  window.Dialog = dialog;
 
-})(window, document);
+});
